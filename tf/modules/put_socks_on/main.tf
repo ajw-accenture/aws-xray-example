21f01be58e2c7b9294ad932feb56125548f07907
ajw-accenture <ajw-accenture@users.noreply.github.com>
2021-01-15 13:11:58 -0600

    stub out ZCS project

--- tf/modules/put_socks_on/main.tf
+++ tf/modules/put_socks_on/main.tf
@@ -23,7 +23,7 @@ resource "aws_lambda_function" "put_socks_on_nanoservice" {
   runtime       = "dotnetcore3.1"
   timeout       = 60
 
-  handler       = "Zapatos.Chaussures.Schuhe.Function.PutSocksOn"
+  handler       = "Zapatos.Chaussures.Schuhe.Function.Handler.PutSocksOn"
   filename      = ""
   role          = aws_iam_role.role_for_put_socks_on_nanoservice.arn
 }
