253bcffca8d20ebe2e6f9056e559a7fa647caed9
ajw-accenture <ajw-accenture@users.noreply.github.com>
2021-01-18 14:45:28 -0600

    refactor: add deserializing attribute

--- Services/UpdateEmployee/Function.cs
+++ Services/UpdateEmployee/Function.cs
@@ -4,6 +4,7 @@ namespace UpdateEmployee
 {
     public class Function
     {
+        [Amazon.Lambda.Core.LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
         public void Invoke(object input, ILambdaContext content) {
             content.Logger.LogLine("Hello, world!");
         }
