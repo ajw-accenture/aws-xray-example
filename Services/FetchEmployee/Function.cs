2bca6a71ba357ba22eb819b0926db423ce8648ce
ajw-accenture <ajw-accenture@users.noreply.github.com>
2021-01-18 14:50:24 -0600

    refactor: Class1.cs -> Function.cs

---
+++ Services/FetchEmployee/Function.cs
@@ -0,0 +1,12 @@
+﻿using Amazon.Lambda.Core;
+
+namespace FetchEmployee
+{
+    public class Function
+    {
+        [Amazon.Lambda.Core.LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+        public void Invoke(object input, ILambdaContext content) {
+            content.Logger.LogLine("Hello, world!");
+        }
+    }
+}
